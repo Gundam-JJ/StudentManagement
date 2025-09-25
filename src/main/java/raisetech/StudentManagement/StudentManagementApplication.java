60d135598633404e79e84c7d4bb5bb167dda4fca
Jonosuke <gundam.jrx159@gmail.com>
2025-09-25 20:02:34 +0900

    第17回コミット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,82 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
-@RestController
+
 public class StudentManagementApplication {
 
+  /*
+  @Autowired
+  private StudentRepository repository;
+   */
+
+  /*private String name = "Johoji Jonosuke";
+  private String age = "25";*/
+
+  //localhost:8080
 	public static void main(String[] args) {
-    //localhost:8080
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-@GetMapping("hello")
-  public String hello(){
-    return "Hello, World";
+
+
+  /*
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
+  }
+
+  @GetMapping("/studentsCourses")
+  public List<StudentsCourses> getStudentsCoursesList(){
+    return repository.searchStudentsCourses();
+  }
+   */
+
+
+  //ターミナルコマンド上で「curl "http://localhost:8080/student?name=指定の名前" 」でとりだせるようになる。
+  /*
+@GetMapping("/student")
+  public String getStudent(@RequestParam String name){
+   */
+
+
+    /*Student student = new Student();
+    student setName("Johoji Jonosuke");
+    String name = student.getName();*/
+
+
+  /*
+  Student student = (Student) repository.searchByName(name);
+  return student.getName() + " " + student.getAge() + "歳";
+*/
+
+
+//Get Post
+//GETは取得、リクエストの結果を受け取る
+//POSTは情報を与える、渡す
+
+  /*
+@PostMapping("/student")//登録
+  public void registerStudent(String name,int age){
+    repository.registerStudent(name, age);
 }
+
+@PatchMapping("/student")//更新
+  public void updateStudentName(String name, int age){
+    repository.updateStudent(name, age);
+}
+
+@DeleteMapping("/student")//削除
+  public void deleteStudent(String name){
+    repository.deleteStudent(name);
+}
+   */
 }
---
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -0,0 +1,40 @@
+package raisetech.StudentManagement.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//lombokでgetter,setterの設定をしてくれる。
+@Getter
+@Setter
+public class Student {
+
+  private String id;
+  private String name;
+  private String kanaName;
+  private String nickname;
+  private String email;
+  private String area;
+  private int age;
+  private String sex;
+
+/*
+  private String name;
+  private int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+*/
+}
